482c0cc3b235b9be1e3e56f68c2475d624981a92
tharsila <tharsilapaula@gmail.com>
2022-10-31 21:59:55 -0300

    feat: create products page

--- src/pages/CreateProducts/index.jsx
+++ src/pages/CreateProducts/index.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Title from '../../components/Title'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+import '../CreateUsers/styles.css'
 
 const CreateProducts = () => {
   return (
-    <h1>CreateProducts</h1>
+    <>
+      <Title text='Cadastrar Produto'/>
+      <main>
+        <div className="formContainer">
+          <form >
+            <Input 
+              type='text' 
+              text='Nome' 
+              name='productName' 
+              placeholder='Digite um nome...'
+            />
+            <Input 
+              type='number' 
+              text='Preço' 
+              name='Price' 
+              placeholder='Digite um preço...'
+            />
+            <Input 
+              type='text' 
+              text='Imagem' 
+              name='image' 
+              placeholder='Envie uma imagem (url)...'
+            />
+            <Button text='Cadastrar' />
+          </form>
+        </div>
+      </main>
+    </>
   )
 }
 
