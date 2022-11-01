1f7e063c8d6a01788c6319b00f10ffb1a47e6914
tharsila <tharsilapaula@gmail.com>
2022-10-31 21:52:23 -0300

    feat: create user page

--- src/pages/CreateUsers/index.jsx
+++ src/pages/CreateUsers/index.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import './styles.css'
 
+import Title from '../../components/Title'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
 const CreateUsers = () => {
   return (
-    <h1>CreateUsers</h1>
+    <>
+      <Title text='Criar Usuários'/>
+      <main>
+        <div className="formContainer">
+          <form >
+            <Input 
+              type='text' 
+              text='Nome' 
+              name='username' 
+              placeholder='Digite um nome...'
+            />
+            <Input 
+              type='email' 
+              text='Email' 
+              name='email' 
+              placeholder='Digite um email...'
+            />
+            <Input 
+              type='password' 
+              text='Senha' 
+              name='password' 
+              placeholder='Digite uma senha...'
+            />
+            <Button text='Criar' />
+          </form>
+
+        </div>
+      </main>
+    </>
   )
 }
 
