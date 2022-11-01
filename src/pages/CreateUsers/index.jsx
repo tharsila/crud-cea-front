50981868d2072c32be7bb4277dc70e10e519e449
tharsila <tharsilapaula@gmail.com>
2022-11-01 14:20:43 -0300

    feat: post and get users

--- src/pages/CreateUsers/index.jsx
+++ src/pages/CreateUsers/index.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
 import Title from '../../components/Title'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
+import { useIndex } from '../../hooks/pages/Users/useIndex'
+
 const CreateUsers = () => {
+  const { 
+    name,
+    email, 
+    password,
+    registerUser,
+    setName,
+    setEmail,
+    setPassword,
+  } = useIndex()
+  
   return (
     <>
       <Title text='Cadastrar Usuários'/>
@@ -17,20 +29,26 @@ const CreateUsers = () => {
               text='Nome' 
               name='username' 
               placeholder='Digite um nome...'
+              value={name}
+              handleOnChange={((e) => setName(e.target.value))}
             />
             <Input 
               type='email' 
               text='Email' 
               name='email' 
               placeholder='Digite um email...'
+              value={email}
+              handleOnChange={((e) => setEmail(e.target.value))}
             />
             <Input 
               type='password' 
               text='Senha' 
               name='password' 
               placeholder='Digite uma senha...'
+              value={password}
+              handleOnChange={((e) => setPassword(e.target.value))}
             />
-            <Button text='Cadastrar' />
+            <Button text='Cadastrar' handleOnClick={registerUser} />
           </form>
 
         </div>
