5104f49fb15216afedae3444d2585488ad221dd1
tharsila <tharsilapaula@gmail.com>
2022-10-31 22:41:02 -0300

    feat: user and products page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,67 @@
 import React from 'react'
+import Title from '../../components/Title'
+import { FaTrashAlt, FaPencilAlt } from 'react-icons/fa'
 import './styles.css'
 
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <Title text='Painel de Produtos' />
+      <div className="tableContainer">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nome</th>
+              <th>Produto</th>
+              <th>Imagem</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Celular 2022</td>
+              <td>R$ 5000</td>
+              <td>https://imagem.com</td>
+              <td className='tableAction'>
+                <span><FaPencilAlt /></span>
+                <span><FaTrashAlt /></span>
+              </td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Celular 2022</td>
+              <td>R$ 5000</td>
+              <td>https://imagem.com</td>
+              <td className='tableAction'>
+                <span><FaPencilAlt /></span>
+                <span><FaTrashAlt /></span>
+              </td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Celular 2022</td>
+              <td>R$ 5000</td>
+              <td>https://imagem.com</td>
+              <td className='tableAction'>
+                <span><FaPencilAlt /></span>
+                <span><FaTrashAlt /></span>
+              </td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Celular 2022</td>
+              <td>R$ 5000</td>
+              <td>https://imagem.com</td>
+              <td className='tableAction'>
+                <span><FaPencilAlt /></span>
+                <span><FaTrashAlt /></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import Title from '../../components/Title'
+import { FaTrashAlt, FaPencilAlt } from 'react-icons/fa'
+import '../Home/styles.css'
 
 const Users = () => {
   return (
-    <h1>Users</h1>
+    <>
+      <Title text='Painel de Usuários' />
+      <div className="tableContainer">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nome</th>
+              <th>Email</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Teste</td>
+              <td>teste@email.com</td>
+              <td className='tableAction'>
+                <span><FaPencilAlt /></span>
+                <span><FaTrashAlt /></span>
+              </td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Teste</td>
+              <td>teste@email.com</td>
+              <td className='tableAction'>
+                <span><FaPencilAlt /></span>
+                <span><FaTrashAlt /></span>
+              </td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Teste</td>
+              <td>teste@email.com</td>
+              <td className='tableAction'>
+                <span><FaPencilAlt /></span>
+                <span><FaTrashAlt /></span>
+              </td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>Teste</td>
+              <td>teste@email.com</td>
+              <td className='tableAction'>
+                <span><FaPencilAlt /></span>
+                <span><FaTrashAlt /></span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
